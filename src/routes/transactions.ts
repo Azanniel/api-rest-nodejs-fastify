5fb4fcf8799c4e892f661f8ee457b36fed5fa847
azanniel <leo.azannielttt@gmail.com>
2023-04-13 15:48:10 -0400

    🔖 add a explanation about hooks in fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,11 @@ import { knex } from '../database'
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  // Exemplo de um middleware global por contexto
+  // app.addHook('preHandler', async (request, reply) => {
+  //   console.log(`[${request.method}] ${request.url}`)
+  // })
+
   app.get(
     '/',
     {
