e865b8a9061882bd03088f843f08a54549dee7b6
azanniel <leo.azannielttt@gmail.com>
2023-04-13 21:06:38 -0400

    🧪 add a testing to create and list transactions

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -12,12 +12,37 @@ describe('Transaction routes', () => {
   })
 
   it('should be able to create a new transaction', async () => {
-    const response = await request(app.server).post('/transactions').send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 
-    expect(response.statusCode).toBe(201)
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(listTransactionsResponse.statusCode).toBe(200)
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
